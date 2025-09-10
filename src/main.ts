bbdded3cb49142ce38e6007b99fadcb12110a501
Viktor Bachmanov <vbachmanov@mail.ru>
2025-09-10 17:01:07 +0300

    install 'vuetify' package

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,25 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'dark'
+  }
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(vuetify)
 
 app.mount('#app')
